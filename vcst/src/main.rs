11f761a748fc7b1dd8a87c3159da4fd2dca764ec
Jonathan Zacsh <j@zac.sh>
2024-12-21 17:16:32 -0600

    basic calp CLI setup; pretty shockingly easy thansk to macros

--- vcst/src/main.rs
+++ vcst/src/main.rs
@@ -1,5 +1,17 @@
+use clap::Parser;
+
+/// Version Control System (VCS) inspection tool.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct VcstArgs {
+    /// Directory for which you'd like to ask VCS questions.
+    // TODO: can swithc to std::path::Path instead?
+    #[arg(short, long)]
+    dir: String,
+}
+
 fn main() {
-    println!("zomg write some calls for $PWD already!");
+    println!("zomg write some calls for $PWD already!\n\t{:?}", VcstArgs::parse());
     println!("\tDO NOT SUBMIT setup cargo CLI crate (clippy?)!");
     println!("\tin the meantime, here's whether you're in a VCS repo:");
 }
