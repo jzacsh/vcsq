a7271739b70ff6bbfabc3c34d99eaef9b7659c25
Jonathan Zacsh <j@zac.sh>
2024-12-30 01:36:06 -0600

    noop(e2e test infra) get access to dirs after `Once` trickery, and callout a few more test-cases as `todo!()`s

--- vcst/tests/integration_test.rs
+++ vcst/tests/integration_test.rs
@@ -1,4 +1,6 @@
 mod common;
+use crate::common::TestDirs;
+use std::process::exit;
 use std::sync::Once;
 
 static ONE_REPO_SETUP: Once = Once::new();
@@ -10,33 +12,62 @@ static ONE_REPO_SETUP: Once = Once::new();
 /// ```rust
 /// cargo test -- --nocapture
 /// ```
-fn setup_tests() {
-    // TODO: (rust) how to capture the mktemp root out of this? we basically need
-    // setup_temp_repos() to return all three tempdirs it made (one PathBuf for each VCS repo
-    // path).
+fn setup_tests() -> TestDirs {
+    use crate::common::make_test_temp::mktemp;
+
+    let testdir_basename = "vcst-e2e-testdirs";
     ONE_REPO_SETUP.call_once(|| {
-        let tmpdir_root = crate::common::mktemp("vcst-e2e-testdirs").expect("setting up test dir");
-        eprintln!("test setup: {:?}", tmpdir_root.clone());
-        crate::common::setup_temp_repos(&tmpdir_root).expect("setup temp repos");
+        let tmpdir_root = mktemp(testdir_basename).expect("setting up test dir");
+        eprintln!("SETUP: {:?}", tmpdir_root.clone());
+        match TestDirs::create(&tmpdir_root) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("test harness fail: {}", e);
+                exit(1);
+            }
+        }
     });
+
+    // TODO: (rust) how to capture the mktemp root out of this? we basically need
+    // create() to return all three tempdirs it made (one PathBuf for each VCS repo
+    // path).
+    TestDirs::new(testdir_basename).expect("failed listing tempdirs")
 }
 
 mod brand {
     #[test]
     fn git() {
-        crate::setup_tests();
+        let test_dir = crate::setup_tests().git_repo;
         assert_eq!(42, 42);
     }
 
     #[test]
     fn hg() {
-        crate::setup_tests();
+        let test_dir = crate::setup_tests().hg_repo;
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn jj() {
+        let test_dir = crate::setup_tests().jj_repo;
         assert_eq!(42, 42);
     }
 
     #[test]
     fn novcs() {
-        crate::setup_tests();
+        let _ = crate::setup_tests().not_vcs; /* DO NOT SUBMIT - add plain_dir */
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn non_dir() {
+        let _ = crate::setup_tests().not_dir; /* DO NOT SUBMIT - add plain_dir */
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn non_extant() {
+        let mut non_extant = crate::setup_tests().non_extant();
         assert_eq!(42, 42);
     }
 }
@@ -44,19 +75,37 @@ mod brand {
 mod root {
     #[test]
     fn git() {
-        crate::setup_tests();
+        let test_dir = crate::setup_tests().git_repo;
         assert_eq!(42, 42);
     }
 
     #[test]
     fn hg() {
-        crate::setup_tests();
+        let test_dir = crate::setup_tests().hg_repo;
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn jj() {
+        let test_dir = crate::setup_tests().jj_repo;
         assert_eq!(42, 42);
     }
 
     #[test]
     fn novcs() {
-        crate::setup_tests();
+        let _ = crate::setup_tests().not_vcs; /* DO NOT SUBMIT - add plain_dir */
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn non_dir() {
+        let _ = crate::setup_tests().not_dir; /* DO NOT SUBMIT - add plain_dir */
+        assert_eq!(42, 42);
+    }
+
+    #[test]
+    fn non_extant() {
+        let mut non_extant = crate::setup_tests().non_extant();
         assert_eq!(42, 42);
     }
 }
