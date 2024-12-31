52a7e45182ec9f9008d8215f313565946494b804
Jonathan Zacsh <j@zac.sh>
2024-12-31 00:25:57 -0600

    noop(test cov) dirty-files should report empty for clean state

--- vcst/tests/cli_test/cmd_dirty.rs
+++ vcst/tests/cli_test/cmd_dirty.rs
@@ -9,17 +9,30 @@ fn git() {
 }
 
 #[test]
-fn hg() {
+fn hg_clean() {
     let test_dirs = TestDirs::create_once();
     let test_dir = test_dirs.hg_repo;
 
+    let mut cmd = Command::cargo_bin("vcst").unwrap();
+    let assert = cmd.arg("dirty-files").arg(&test_dir).assert();
+    assert
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::is_empty());
+
+    //
+    // Arrange: make the repo dirty
+    //
     let mut untracked_file = test_dir.clone();
     untracked_file.push("MY_DOC.md");
 
     let _ = make_test_temp::touch(&untracked_file).expect("test arrange: touch failed");
 
+    //
+    // Assert: dirty repo how has report of what's dirty
+    //
     let mut cmd = Command::cargo_bin("vcst").unwrap();
-    let assert = cmd.arg("dirty-files").arg(test_dir).assert();
+    let assert = cmd.arg("dirty-files").arg(&test_dir).assert();
     assert
         .success()
         .stdout(predicate::str::diff("? MY_DOC.md\n"))
