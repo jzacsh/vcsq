ef046977a0980be790cf0dcbf9067ffabd6ff226
Jonathan Zacsh <j@zac.sh>
2025-01-02 20:46:42 -0600

    noop(clippy pedantic) testable-main should take a reference to its args; https://rust-lang.github.io/rust-clippy/master/index.html#needless_pass_by_value

--- vcst/src/main.rs
+++ vcst/src/main.rs
@@ -4,6 +4,6 @@ use std::process::exit;
 use vcst::{vcst_query, VcstArgs};
 
 fn main() {
-    let exit_code = vcst_query(VcstArgs::parse(), &mut io::stdout(), &mut io::stderr());
+    let exit_code = vcst_query(&VcstArgs::parse(), &mut io::stdout(), &mut io::stderr());
     exit(exit_code.into());
 }
