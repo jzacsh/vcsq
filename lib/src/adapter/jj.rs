a989eebb746a87e300c0809a7584601e608c199e
Jonathan Zacsh <j@zac.sh>
2025-01-06 00:48:07 -0600

    feature(jj) current-id subcmd implemented

--- lib/src/adapter/jj.rs
+++ lib/src/adapter/jj.rs
@@ -1,6 +1,6 @@
 use crate::repo::{
     DirPath, Driver, DriverError, HistoryRefId, HistoryRefName, Validator, VcsAvailable,
-    ERROR_REPO_NOT_DIRTY,
+    ERROR_REPO_NOT_CLEAN, ERROR_REPO_NOT_DIRTY,
 };
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
@@ -73,6 +73,18 @@ impl Repo {
         cmd.arg("file").arg("list").arg("-r").arg("@-");
         cmd
     }
+
+    fn jj_current_ref_id(&self) -> Command {
+        let mut cmd = self.start_shellout();
+        cmd.arg("log")
+            .arg("--color=never")
+            .arg("--no-graph")
+            .arg("--revisions")
+            .arg("@-")
+            .arg("--template")
+            .arg("commit_id");
+        cmd
+    }
 }
 
 impl Driver for Repo {
@@ -106,10 +118,21 @@ impl Driver for Repo {
         Ok(files)
     }
 
-    fn current_ref_id(&self, _dirty_ok: bool) -> Result<HistoryRefId, DriverError> {
-        todo!(); // TODO: (feature) implement
+    /// Returns the backing store's "commit id" (as opposed to the more ephemeral "change id").
+    fn current_ref_id(&self, dirty_ok: bool) -> Result<HistoryRefId, DriverError> {
+        if !dirty_ok && !self.is_clean()? {
+            return Err(ERROR_REPO_NOT_CLEAN.to_string().into());
+        }
+        let output =
+            DriverError::expect_cmd_lossy("jj cli".to_string(), self.jj_current_ref_id().output())?;
+        DriverError::expect_cmd_line("jj cli", &output)
     }
 
+    /// Returns the current bookmark if there one.
+    // TODO: (feature) when jj is more stable, do more advanced things: determine if we're
+    // git-backed, and then translate our answers into the answe ra user would expect if this
+    // iwasn't jj-on-git but just git. For now we just keep it sipmle (and less featureful) then
+    // our git counterpart driver.
     fn current_ref_name(&self, _dirty_ok: bool) -> Result<Option<HistoryRefName>, DriverError> {
         todo!(); // TODO: (feature) implement
     }
